5604c4c2b66237b8a773df3d8e1dffa3e886ee90
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-04 17:26:41 -0600

    Finished component to render dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {SearchDynamicComponentModel} from './models/search-dynamic-component.model';
+import {IconBaseComponent} from './components/icons/base/icon-base.component';
+import {IconsCollectionClass} from './collection/icons-collection.class';
+import {DeleteNodeEventService} from './services/event/delete-node.event.service';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +10,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'dynamic-components';
+  private number: number;
+  private iconCollection: IconsCollectionClass;
 
-  constructor() {}
+  model: SearchDynamicComponentModel<IconBaseComponent>;
+
+  constructor(
+    private deleteNodeEventService: DeleteNodeEventService
+  ) {
+    this.iconCollection = new IconsCollectionClass();
+    this.model = new SearchDynamicComponentModel<IconBaseComponent>( this.iconCollection.collection, 0 );
+  }
 
   ngOnInit(): void { }
+
+  changeNumber() {
+    this.number = Math.round( Math.random() * 5 );
+    this.model = { ...this.model, id: this.number };
+  }
+
+  deleteSvg() {
+    this.deleteNodeEventService.event.emit( true );
+  }
 
 }
